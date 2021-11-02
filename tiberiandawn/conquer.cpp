19495fc7c7f08dec82ae3bc8b4eadb0c329568b3
Giuliano Belinassi <giuliano.belinassi@gmail.com>
2021-11-02 17:46:08 -0300

    [NDSi] Skip video if left mouse is clicked

--- tiberiandawn/conquer.cpp
+++ tiberiandawn/conquer.cpp
@@ -2930,7 +2930,7 @@ int VQ_Call_Back(unsigned char*, int)
     Interpolate_2X_Scale(&SysMemPage, &SeenBuff, NULL, Settings.Video.InterpolationMode);
     Frame_Limiter();
 
-    if ((BreakoutAllowed || Debug_Flag) && key == KN_ESC) {
+    if ((BreakoutAllowed || Debug_Flag) && (key == KN_ESC || key == VK_LBUTTON)) {
         WWKeyboard->Clear();
         Brokeout = true;
         return (true);
