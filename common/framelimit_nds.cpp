63673955f894d5a52ee0b8fe4c1c6ee841f2588c
Giuliano Belinassi <giuliano.belinassi@gmail.com>
2021-11-07 12:30:08 -0300

    [NDSi] Fix glitch on sound queue

--- common/framelimit_nds.cpp
+++ common/framelimit_nds.cpp
@@ -12,24 +12,5 @@ void Update_HWCursor();
 
 void Frame_Limiter(bool force_render)
 {
-    static unsigned ticks = 0;
-    static unsigned ticks_start;
-
-    if (ticks == 0) {
-        timerStart(1, ClockDivider_1024, 0, NULL);
-        ticks += timerElapsed(1);
-        ticks_start = ticks;
-    }
-
-    ticks += timerElapsed(1);
-    unsigned now = ticks;
-    unsigned ticks_dt = now - ticks_start;
-
-    if (ticks_dt < 30) {
-        // If FPS is too high the mouse glitches out, so we force
-        // VSync in this case.
-        swiWaitForVBlank();
-    }
     Update_HWCursor();
-    ticks_start = ticks;
 }
