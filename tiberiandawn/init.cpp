c88dad2faedeee3cda08c685ca72eec14f80474a
Giuliano Belinassi <gbelinassi@suse.de>
2022-06-29 16:14:57 -0300

    [TD] Clean ShadowPage
    ShadowPage is a buffer that was designed to be used to avoid accessing
    video memory. However, it is allocated but never really used. Clean
    it up, saving ~64k of RAM.
    
    Signed-off-by: Giuliano Belinassi <gbelinassi@suse.de>

--- tiberiandawn/init.cpp
+++ tiberiandawn/init.cpp
@@ -633,8 +633,6 @@ bool Init_Game(int, char*[])
 //#ifndef NOMEMCHECK
 void Uninit_Game(void)
 {
-    delete Map.ShadowPage;
-    Map.ShadowPage = NULL;
     Map.Free_Cells();
 
     delete[] SpeechBuffer;
